38b83fe98d8df5d76fb420cb40264bedbf4a7137
Shiv Matta <shiv.matta@gmail.com>
2022-06-14 22:18:16 -0500

    commit tree 95fdbf6e9554fddfbc704a54f7ac6a3ec579cb92

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"];
+event = 'surprise'
+
+function writeCards(names, event){
+    let messages = [];
+    for(let i = 0;i<names.length; i++){
+    
+     messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+function countDown(num){
+    let i = num; 
+    while (i>= 0) {
+console.log(i--)
+    }
+   
+}
